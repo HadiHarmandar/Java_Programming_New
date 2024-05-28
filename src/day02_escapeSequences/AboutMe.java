7723ccefc00e54267dea68839c7f79de47b70adc
HadiHarmandar <hadiharmandar1@gmail.com>
2024-05-28 12:08:40 -0500

    Exercise

---
+++ src/day02_escapeSequences/AboutMe.java
@@ -0,0 +1,20 @@
+package day02_escapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("\tMy favourite book is \"The Rich Dad and The Poor Dad\"\n\tMy favourite quote is \"No Pain No Gain\"\n" +
+                "\tMy favourite TV show is \"Breaking Bad\"\n\tMy favorite music is pop music");
+
+    }
+}
+
+/**
+    Create a class named AboutMe.java
+    Write a code to print the following info about you:
+        1. Your favorite book
+        2. Your favorite quote
+        3. Your favorite TV show
+        4. Your favorite music
+ */
