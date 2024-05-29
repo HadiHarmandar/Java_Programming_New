db51596707e2b7425a51220aa5dd7ba7bf419042
HadiHarmandar <hadiharmandar1@gmail.com>
2024-05-29 09:33:54 -0500

    Warm up

---
+++ src/day04_concatenation/PersonalInfo.java
@@ -0,0 +1,47 @@
+package day04_concatenation;
+
+public class PersonalInfo {
+
+    public static void main(String[] args) {
+
+        String firstName = "Aaron",
+                lastName = "Daniel",
+                from = "United Kingdom",
+                favourite_music = "Rock musics",
+                favourite_book = "Data Structures in Java",
+                favouriteTvShow = "Breaking Bad",
+                favouriteColor = "Blue";
+        char gender = 'M';
+        int age = 32;
+        boolean isMarried = true,
+                isEmployed = false;
+
+        System.out.println("firstName = " + firstName);
+        System.out.println("lastName = " + lastName);
+        System.out.println("gender = " + gender);
+        System.out.println("age = " + age);
+        System.out.println("from = " + from);
+        System.out.println("favourite_music = " + favourite_music);
+        System.out.println("favourite_book = " + favourite_book);
+        System.out.println("favouriteTvShow = " + favouriteTvShow);
+        System.out.println("favouriteColor = " + favouriteColor);
+        System.out.println("isMarried = " + isMarried);
+        System.out.println("isEmployed = " + isEmployed);
+
+    }
+}
+
+/**
+Create a class named PersonalInfo, and declare the variables with the appropriate for the followings:
+     1. first name
+     2. last name
+     3. gender
+     4. age
+     5. from
+     6. favorite music
+     7. favorite book
+     8. favorite show
+     9. favorite color
+     10. married status
+     11. employee status
+ */
