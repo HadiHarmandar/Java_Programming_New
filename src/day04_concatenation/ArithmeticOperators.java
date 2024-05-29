2382e0e49277e0434f8fa360a9ac1e1e928f9c0c
HadiHarmandar <hadiharmandar1@gmail.com>
2024-05-29 14:27:50 -0500

    ArithmeticOperators

---
+++ src/day04_concatenation/ArithmeticOperators.java
@@ -0,0 +1,32 @@
+package day04_concatenation;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        System.out.println(100 + 200);
+
+        System.out.println(40 - 10);
+
+        System.out.println(50 / 6); // 8
+
+        System.out.println(50 / 6.); // 8.333...
+
+        System.out.println(50.0 / 6); // 8.333...
+
+        System.out.println(50D / 6); // 8.333...
+
+        System.out.println((int) (2.5 / 0.5)); // 5
+
+//        System.out.println(9 / 0); // ArithmeticException
+
+        System.out.println(10 * 2);
+
+        System.out.println(20 * 12);
+
+        System.out.println(100 % 13);
+
+        System.out.println(45 % 8);
+
+    }
+}
